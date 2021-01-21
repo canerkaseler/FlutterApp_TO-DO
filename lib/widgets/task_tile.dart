26caf3f7cdeb78ff11606f2151c31caad76d3eeb
canerkaseler <caner.kaseler@enqura.com>
2021-01-22 01:16:14 +0300

    Checkbox and text has states.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
 
-class TaskTitle extends StatelessWidget {
+class TaskTitle extends StatefulWidget {
+
+  @override
+  _TaskTitleState createState() => _TaskTitleState();
+}
+
+class _TaskTitleState extends State<TaskTitle> {
+
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
-    return CheckboxListTile(value: false, title: Text("Buy milk"),);
+    return ListTile(
+      title: Text("This is a task", style: TextStyle(
+        decoration: isChecked ? TextDecoration.lineThrough : null
+      ),),
+      trailing: TaskCheckbox(
+        isChecked: isChecked,
+        checkboxState: (bool newValue){
+          setState(() {
+            isChecked = newValue;
+          });
+        }
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+
+  final bool isChecked;
+  final Function checkboxState;
+
+  TaskCheckbox({this.isChecked, this.checkboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: checkboxState,
+    );
   }
 }
